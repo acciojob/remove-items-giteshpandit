1399e962386798d5edf8524eaf3a1ad59df2ae1a
giteshpandit <panditgitesh38@gmail.com>
2024-09-07 19:33:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById('removeBtn').addEventListener('click', function() {
+  // Get the select element
+  const colorSelect = document.getElementById('colorSelect');
+  
+  // Get the selected option
+  const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+  
+  // Remove the selected option
+  colorSelect.removeChild(selectedOption);
+});
+
